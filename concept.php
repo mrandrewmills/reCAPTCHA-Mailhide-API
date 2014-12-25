9be9a355eb6d9c0020359b10ba9e40ce3c581a7c
mrandrewmills <andrewmills@knowwwhow.com>
2014-12-25 15:51:40 -0500

    add email padding code
    add code to pad email address up to next 16-byte boundary

--- concept.php
+++ concept.php
@@ -3,7 +3,11 @@
 function encryptEmailAddress($emailAddress, $secretKey){
 
 			// pad email to 16-byte boundary
-			
+			$block_size = 16;
+			$numpad = ( $block_size - ( strlen( $emailAddress ) % $block_size ) );
+			$filler = chr( $numpad );
+			$emailAddress = $emailAddress . str_repeat( $filler, $numpad );
+
 			// encrypt email address with AES-128-CBC
 
 			// convert to URl-safe base64
